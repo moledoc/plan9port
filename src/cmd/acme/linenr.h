b702bce9a33cefd100cd8e6fc8fa7f989d5ca662
utt <meelis.utt@gmail.com>
2026-05-26 22:57:19 +0200

    improve .h comments and calc

--- src/cmd/acme/linenr.h
+++ src/cmd/acme/linenr.h
@@ -1,8 +1,12 @@
 #ifndef LINENR_H
 #define LINENR_H
 
+// NOTE: Linenrwid makes room for number containing 'Linenrwid_len' digits for rownumber drawing.
+// NOTE: /2 because stringwid("0") gives 2 with the fonts I tried.
+// NOTE: defaults to 32 if no access to font. With the font I'm currently running, it should be enough (3 digits).
 #define Linenrwid_len 3
-#define Linenrwid scalesize(display, t && t->fr.font ? (Linenrwid_len+1)*stringwidth(t->fr.font, "0")/2 : 32) // NOTE: fit Linenrwid_seed nr of digit in the number; /2, because stringwidth(,"0") will fit 2 instead of 1; +1 because with this func otherwise fits seed-1. // NOTE: requires access to Text *t.
+#define Linenrwid scalesize(display, t && t->fr.font ? (Linenrwid_len+(Linenrwid_len%2!=0))*stringwidth(t->fr.font, "0")/2 : 32)
+// #define Linenrwid scalesize(display,  32)
 
 void linenrdraw(Text *);
 
