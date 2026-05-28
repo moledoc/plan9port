930ec0723b0eaba25d3ea722501ed33224a7db08
utt <meelis.utt@gmail.com>
2026-05-28 18:52:55 +0200

    fixes
    add some keywords to sql
    codetags skip switch statement, example with SQL NOTE and NOT - lev distance will un syhl NOTE.

--- src/cmd/acme/tsyhl.c
+++ src/cmd/acme/tsyhl.c
@@ -133,6 +133,7 @@ void _bsyhl(Frame *f, Point pt, Frbox *b, uint p0, uint p1, int extension, enum 
 			if (in_word_set_codetags(buf, buf_len)) {
 				text = syhlcols[SYHL_CODETAG];
 				should_draw = 1;
+				goto AfterSwitch;
 			} else if (action == SYHL_ACTION_TYPING) {
 				lev_dist_1((char **)keywords_codetags, sizeof(keywords_codetags)/sizeof(keywords_codetags[0]), buf, &text, &should_draw);
 			}
@@ -188,6 +189,7 @@ void _bsyhl(Frame *f, Point pt, Frbox *b, uint p0, uint p1, int extension, enum 
 					break;					
 			}
 
+			AfterSwitch:
 			// NOTE: we passed end; step back
 			offset--; ptr--;
 
